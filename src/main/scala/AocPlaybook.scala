eeda8a15ccdab442e17e613f0cf1a29a2dfccd55
Hidde Dorhout <hidde.dorhout@cleverbase.com>
2021-12-06 20:05:00 +0100

    day 3

--- src/main/scala/AocPlaybook.scala
+++ src/main/scala/AocPlaybook.scala
@@ -23,8 +23,8 @@ object AocPlaybook extends App {
     }
   }
 
-  println(s"Number of increases in depth is ${countIncreases(depthReport)}")
-  println(s"Number of window sum increases in depth is ${countIncreases(depthReport, 3)}")
+//  println(s"Number of increases in depth is ${countIncreases(depthReport)}")
+//  println(s"Number of window sum increases in depth is ${countIncreases(depthReport, 3)}")
 
   // day2
   println("DAY 2")
@@ -62,7 +62,7 @@ object AocPlaybook extends App {
       }
     })
 
-  println(s"The sum of the location is: ${result_part1._1 * result_part1._2}")
+//  println(s"The sum of the location is: ${result_part1._1 * result_part1._2}")
 
   val aimStart = 0
 
@@ -76,6 +76,62 @@ object AocPlaybook extends App {
       }
     })
 
-  println(s"The sum of the location based on aim is: ${result_part2._1 * result_part2._2}")
+//  println(s"The sum of the location based on aim is: ${result_part2._1 * result_part2._2}")
+
+  // day3
+  println("DAY 3")
+
+  def diagnosticDecoder(in: String): List[String] = in.split("").toList
+
+  val diagnosticReport = readFile("diagnosticReport")(diagnosticDecoder)
+
+  val powerConsumption = diagnosticReport.transpose
+    .map(getMostAndLeastAvailable)
+    .transpose
+    .map(listOfBinaryValuesToInt)
+    .product
+
+  def getMostAndLeastAvailable(l: List[String]): List[String] = {
+    if (l.count(_ == "0") > l.count(_ == "1")) List("0", "1") else List("1", "0")
+  }
+
+  def getMostAndLeastAvailable1(l: List[String]): List[String] = {
+    if (l.count(_ == "1") > l.count(_ == "0")) List("0", "1") else List("1", "0")
+  }
+
+  def listOfBinaryValuesToInt(in: List[String]): Int = Integer.parseInt(in.mkString(""), 2)
+
+  println(s"The power consumption of the submarine is ${powerConsumption}")
+
+  def toFilteredList(next: List[List[String]],
+                     criteriaOps: List[List[String]] => List[String])(index: Int): List[List[String]] = {
+    if (next.length != 1) {
+      val filtered = next.filter(l => {
+        val indexToCheck = l.length - index
+        l(indexToCheck) == criteriaOps(next)(indexToCheck)
+      })
+      toFilteredList(filtered, criteriaOps)(index - 1)
+    } else {
+      next
+    }
+  }
+
+  def oxygenCriteria(in: List[List[String]]): List[String] =
+    in.transpose.map(l => {
+      if (l.count(_ == "0") > l.count(_ == "1")) "0" else "1"
+    })
+
+  def co2Criteria(in: List[List[String]]): List[String] =
+    in.transpose.map(l => {
+      if (l.count(_ == "1") >= l.count(_ == "0")) "0" else "1"
+    })
+
+  lazy val oxygenRating = listOfBinaryValuesToInt(
+    toFilteredList(diagnosticReport, oxygenCriteria)(diagnosticReport.head.length).head
+  )
+  lazy val co2Rating = listOfBinaryValuesToInt(
+    toFilteredList(diagnosticReport, co2Criteria)(diagnosticReport.head.length).head
+  )
+  println(s"The life support rating of the submarine is ${oxygenRating * co2Rating}")
 
 }
